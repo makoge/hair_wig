7a69354e14d0b757789d4b8fca572e44327562af
makoge <makogeryan@gmail.com>
2025-12-20 19:26:12 +0200

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { CartProvider } from "./context/CartContext";
