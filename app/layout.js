76bec494cf71346a211ffc59f3c1444f9bf89ed1
makoge <makogeryan@gmail.com>
2025-12-21 21:35:42 +0200

    added products

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
         <CartProvider>
         <Header/>
         {children}
+        <Analytics/>
         <Footer/>
         </CartProvider>
         </body>
