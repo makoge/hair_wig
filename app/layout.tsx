b2f73ab9e3c5af982109458c4abd66bc3b015e20
makoge <makogeryan@gmail.com>
2026-02-18 11:23:42 +0200

    contents

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
   description:
     "Shop premium human hair wigs, lace front wigs, and monofilament wigs at Confida Lace Hair. Natural look, breathable comfort, and luxury quality at affordable prices.",
   metadataBase: new URL("https://confida.shop"),
+   verification: {
+    other: {
+       "msvalidate.01":"F92AC11091A9F71912C4635E1FCF1FDE",
+    },
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
